5b0bd924d82f02344d336603fd259808739afba7
GuidoAquino <140458966+GuidoAquino@users.noreply.github.com>
2023-10-30 15:31:12 -0300

    Update Nav.js

--- src/Components/Molecules/Nav.js
+++ src/Components/Molecules/Nav.js
@@ -52,12 +52,12 @@ const Nav = () => {
                 >
                   Calendario
                 </Link>
-                <Link
-                  href="#inscripcion"
-                  className="colorrojo hover:bg-gradient-to-t from-[#000000] to-[#373737] hover:text-white rounded-lg p-2"
-                >
-                  Inscripcion
-                </Link>
+                // <Link
+                //   href="#inscripcion"
+                //   className="colorrojo hover:bg-gradient-to-t from-[#000000] to-[#373737] hover:text-white rounded-lg p-2"
+                // >
+                //   Inscripcion
+                // </Link>
                 
                 {/* <Link
                   href="/iniciarsesion"
@@ -129,12 +129,12 @@ const Nav = () => {
                 >
                   Calendario
                 </Link>
-                <Link
-                  href="#inscripcion"
-                  className="colorrojo block hover:bg-gradient-to-t from-[#000000] to-[#373737] hover:text-white rounded-lg p-2"
-                >
-                  Inscripcion
-                </Link>
+                // <Link
+                //   href="#inscripcion"
+                //   className="colorrojo block hover:bg-gradient-to-t from-[#000000] to-[#373737] hover:text-white rounded-lg p-2"
+                // >
+                //   Inscripcion
+                // </Link>
                 
                 {/* <Link
                   href="/iniciarsesion"
