d2ff18a28389cb898c2a05ecc923fdff67f1278f
GuidoAquino <140458966+GuidoAquino@users.noreply.github.com>
2023-10-30 15:25:52 -0300

    Update FechasCalendario.js

--- src/Components/Molecules/FechasCalendario.js
+++ src/Components/Molecules/FechasCalendario.js
@@ -1,6 +1,3 @@
-
-
-
 import React from 'react'
 import Calendario from '../Atoms/Calendario'
 
@@ -8,9 +5,9 @@ const FechasCalendario = () => {
   return (
     <>
        
-    <section className='h-auto py-8  flex flex-col justify-center items-center  bggris  bg-no-repeat bg-cover bg-center '>
-    <h2 className='mb-5  text-[16px] text-white sm:text-3xl'>Calendario T13</h2>
-    <div className='gap-2 grid grid-rows-12 grid-cols-1 md:grid-rows-4 md:grid-cols-3 lg:grid-rows-3 lg:grid-cols-4  p-auto flex-cols flex-wrap items-center justify-center  sm:grid-rows-6 sm:grid-cols-2 lg:grid-rows-2 lg:grid-cols-6'>
+    <section  className='h-auto flex flex-col justify-center items-center  bggris  bg-no-repeat bg-cover bg-center '>
+    <h2 className='mb-5  text-[16px] text-white sm:text-3xl mt-3'>Calendario T13</h2>
+    <div className='gap-2 pb-3 grid grid-rows-12 grid-cols-1 md:grid-rows-4 md:grid-cols-3  flex-cols flex-wrap items-center justify-center  sm:grid-rows-6 sm:grid-cols-2 lg:grid-rows-2 lg:grid-cols-6'>
     <Calendario/>
   
    
